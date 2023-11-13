55418dd68d5e08de22c74572d76185faf206f44c
mudit088 <muditsri25@gmail.com>
2023-11-13 23:05:54 +0530

    updated

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -1,69 +1,115 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { data } from '../data';
 
 const Filters = () => {
-    const[search,setSearch]=useState('')
-    const data = [
-        { id: 1, name: 'John Doe', email: 'john@example.com' , number :'455353' , Rno : '545' },
-        { id: 2, name: 'Jane Doe', email: 'jane@example.com' , number :'455353' , Rno : '545'},
-        { id: 3, name: 'Bob Smith', email: 'bob@example.com' , number :'455353' , Rno : '545'},
-        { id: 1, name: 'John Doe', email: 'john@example.com' , number :'455353' , Rno : '545' },
-        { id: 2, name: 'Jane Doe', email: 'jane@example.com' , number :'455353' , Rno : '545'},
-        { id: 3, name: 'Bob Smith', email: 'bob@example.com' , number :'455353' , Rno : '545'},
-        { id: 1, name: 'John Doe', email: 'john@example.com' , number :'455353' , Rno : '545' },
-        { id: 2, name: 'Jane Doe', email: 'jane@example.com' , number :'455353' , Rno : '545'},
-        { id: 3, name: 'Bob Smith', email: 'bob@example.com' , number :'455353' , Rno : '545'},
-        { id: 1, name: 'John Doe', email: 'john@example.com' , number :'455353' , Rno : '545' },
-        { id: 2, name: 'Jane Doe', email: 'jane@example.com' , number :'455353' , Rno : '545'},
-        { id: 3, name: 'Bob Smith', email: 'bob@example.com' , number :'455353' , Rno : '545'},
-        { id: 1, name: 'John Doe', email: 'john@example.com' , number :'455353' , Rno : '545' },
-        { id: 2, name: 'Jane Doe', email: 'jane@example.com' , number :'455353' , Rno : '545'},
-        { id: 3, name: 'Bob Smith', email: 'bob@example.com' , number :'455353' , Rno : '545'},
-        { id: 1, name: 'John Doe', email: 'john@example.com' , number :'455353' , Rno : '545' },
-        { id: 2, name: 'Jane Doe', email: 'jane@example.com' , number :'455353' , Rno : '545'},
-        { id: 3, name: 'Bob Smith', email: 'bob@example.com' , number :'455353' , Rno : '545'},
-      ];
-      const filteredData = data.filter((item) =>
-        item.name.toLowerCase().includes(search.toLowerCase()) ||
-        item.email.toLowerCase().includes(search.toLowerCase()) ||
-        item.number.includes(search) ||
-        item.Rno.includes(search)
-    );
-      
-            return (
-                <div>
-                    <div>
-                        <form>
-                        <input type="search"
-                        onChange={(e) => setSearch(e.target.value)}
-                        placeholder="Search here"
-                        className="w-96 p-4 rounded-full ml-44 " />
-                        </form>
-                    </div>
-                <table>
-            <thead>
-                <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>number</th>
-                <th>Rno</th>
-                </tr>
-            </thead>
-            <tbody>
-                {filteredData.map((val,i) => (
-                <tr key={val.i}>
-                    <td>{val.id}</td>
-                    <td>{val.name}</td>
-                    <td>{val.email}</td>
-                    <td>{val.number}</td>
-                    <td>{val.Rno}</td>
-                </tr>
-                ))}
-            </tbody>
-            </table>
-                </div>
-            )
-            }
+  const [search, setSearch] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordsPerPage = 10;
 
-export default Filters
+  const filteredData = data.filter(
+    (item) =>
+      item.Name.toLowerCase().includes(search.toLowerCase()) ||
+      item.email.toLowerCase().includes(search.toLowerCase()) ||
+      item.phone.includes(search) ||
+      item.Rno.includes(search)
+  );
+
+  const lastIndexOfCurrentPage = currentPage * recordsPerPage;
+  const firstIndexOfCurrentPage = lastIndexOfCurrentPage - recordsPerPage;
+  const currentRecords = filteredData.slice(
+    firstIndexOfCurrentPage,
+    lastIndexOfCurrentPage
+  );
+
+  const totalPageCount = Math.ceil(filteredData.length / recordsPerPage);
+  const pageNumbers = [...Array(totalPageCount).keys()].map((num) => num + 1);
+
+  const previousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const changeCurrentPage = (number) => {
+    setCurrentPage(number);
+  };
+
+  const nextPage = () => {
+    if (currentPage < totalPageCount) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  return (
+    <div className="container mx-auto p-8">
+      <div className="mb-4">
+        <form>
+          <input
+            type="search"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search here"
+            className="w-64 bg-teal-500 rounded-full h-12 w-16 rounded-full ml-4 m-6 pl-4"
+          />
+        </form>
+      </div>
+      <table className="min-w-full bg-white border border-gray-300">
+        <thead>
+          <tr className="bg-gray-100">
+            <th className="py-2 px-4">ID</th>
+            <th className="py-2 px-4">Name</th>
+            <th className="py-2 px-4">Email</th>
+            <th className="py-2 px-4">Phone</th>
+            <th className="py-2 px-4">Rno</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentRecords.map((val) => (
+            <tr key={val.ID} className="hover:bg-gray-50">
+              <td className="py-2 px-4">{val.ID}</td>
+              <td className="py-2 px-4">{val.Name}</td>
+              <td className="py-2 px-4">{val.email}</td>
+              <td className="py-2 px-4">{val.phone}</td>
+              <td className="py-2 px-4">{val.Rno}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <nav className="mt-4">
+        <ul className="flex list-none">
+          <li className="mr-2">
+            <button
+              className="bg-teal-500 text-white px-4 py-2 rounded disabled:opacity-50"
+              onClick={previousPage}
+              disabled={currentPage === 1}
+            >
+              Prev
+            </button>
+          </li>
+          {pageNumbers.map((number) => (
+            <li key={number} className="mr-2">
+              <button
+                className={`bg-teal-500 text-white px-4 py-2 rounded ${
+                  currentPage === number ? 'bg-teal-700' : ''
+                }`}
+                onClick={() => changeCurrentPage(number)}
+              >
+                {number}
+              </button>
+            </li>
+          ))}
+          <li>
+            <button
+              className="bg-teal-500 text-white px-4 py-2 rounded disabled:opacity-50"
+              onClick={nextPage}
+              disabled={currentPage === totalPageCount}
+            >
+              Next
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Filters;
