e9f4b1ed98ef093c30a900c2e1da42b6962238ec
mudit088 <muditsri25@gmail.com>
2023-11-28 10:44:07 +0530

    updated

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -8,42 +8,42 @@ const Nav = () => {
       <div className=" pl-20 text-18px">
         <ul className="space-x-4  sm:space-x-8 flex flex-col justify-center items-center sm:flex-row    ">
           <li>
-            <a href="/" className="flex items-center pr-6 p-2 ">
+            <a href="/" className="flex items-center pr-6 p-1 ">
               Home Page
             </a>
           </li>
           <li className="relative group">
-            <a href="#" className="flex items-center p-2   pr-6">
+            <a href="#" className="flex items-center p-1   pr-6">
               Customer Management
             </a>
           </li>
           <li>
-            <a href="/rejection-manager" className="flex items-center p-2  pr-6">
+            <a href="/rejection-manager" className="flex items-center p-1  pr-6">
               Rejection Manager
             </a>
           </li>
           <li>
-            <a href="/loan-book" className="flex items-center p-2  pr-6">
+            <a href="/loan-book" className="flex items-center p-1  pr-6">
               Loan Book
             </a>
           </li>
           <li>
-            <a href="/login-book" className="flex items-center p-2  pr-6">
+            <a href="/login-book" className="flex items-center p-1  pr-6">
               Login Book
             </a>
           </li>
           <li>
-            <a href="/dashboard" className="flex items-center p-2  pr-6">
+            <a href="/dashboard" className="flex items-center p-1  pr-6">
               Dashboard
             </a>
           </li>
           <li>
-            <a href="/post-management" className="flex items-center p-2  pr-6">
+            <a href="/post-management" className="flex items-center p-1  pr-6">
               Post Management
             </a>
           </li>
           <li>
-            <a href="/masters" className="flex items-center p-2  pr-6">
+            <a href="/masters" className="flex items-center p-1  pr-6">
               Masters
             </a>
           </li>
